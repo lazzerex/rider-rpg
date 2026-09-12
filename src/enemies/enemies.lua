6a495e383549c4de04fb733c345c5dd08b7816bf
lazzerex <nambinh236@gmail.com>
2026-09-13 03:37:19 +0700

    feat: add enemies module

---
+++ src/enemies/enemies.lua
@@ -0,0 +1,55 @@
+local Common = require("moves.common")
+
+local Enemies = {}
+
+Enemies.list = {
+    {
+        id = "grunt", name = "Grunt Monster",
+        baseAttack = 16, baseDefense = 16, baseSpeed = 16, baseMaxHP = 40,
+        moves = { Common.Punch, Common.Kick, Common.Guard },
+    },
+    {
+        id = "swift_lurker", name = "Swift Lurker",
+        baseAttack = 14, baseDefense = 10, baseSpeed = 30, baseMaxHP = 34,
+        moves = { Common.Punch, Common.Kick },
+    },
+    {
+        id = "iron_guardian", name = "Iron Guardian",
+        baseAttack = 14, baseDefense = 30, baseSpeed = 8, baseMaxHP = 55,
+        moves = { Common.Punch, Common.Guard, Common.HeavyStrike },
+    },
+    {
+        id = "brute", name = "Brute",
+        baseAttack = 26, baseDefense = 14, baseSpeed = 12, baseMaxHP = 45,
+        moves = { Common.HeavyStrike, Common.Punch },
+    },
+    {
+        id = "toxic_crawler", name = "Toxic Crawler",
+        baseAttack = 12, baseDefense = 14, baseSpeed = 18, baseMaxHP = 38,
+        moves = {
+            Common.Punch,
+            {
+                id = "corrosive_grip", name = "Corrosive Grip", type = "status",
+                power = 8, accuracy = 0.9, energyGain = 8,
+                applyStatus = "weaken",
+                description = "A draining grip that weakens the target's attack.",
+            },
+        },
+    },
+}
+
+function Enemies.random(level)
+    local template = Enemies.list[math.random(#Enemies.list)]
+    return {
+        id = template.id,
+        name = template.name,
+        level = level,
+        forms = { base = { label = "", attack = template.baseAttack, defense = template.baseDefense, speed = template.baseSpeed } },
+        baseFormKey = "base",
+        baseMaxHP = template.baseMaxHP,
+        moves = template.moves,
+        unlocked = {},
+    }
+end
+
+return Enemies
