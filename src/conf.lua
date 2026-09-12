4089a6732e71797964f700d8b978d1eb7cce91b4
lazzerex <nambinh236@gmail.com>
2026-09-13 03:36:02 +0700

    feat: add initial configuration for love2d window

---
+++ src/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.title = "Kamen Rider RPG"
+    t.window.width = 960
+    t.window.height = 640
+    t.window.resizable = true
+    t.console = false
+    t.identity = "rider-rpg"
+end
